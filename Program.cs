1b0257dccebc26db376a1c99e214e7d5cf7f1977
Valeriy <cherkasov77@mail.ru>
2022-08-13 14:49:25 +0400

    Семинар2 задача15ФиналGitHub

--- Program.cs
+++ Program.cs
@@ -33,39 +33,39 @@
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 //и проверяет, является ли этот день выходным.
 
-Console.Clear();
-Console.WriteLine("Введите цифру, обозначающую день недели:  ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number < 1 || number > 7)
-{
-    Console.WriteLine("Введите цифру, от 1 до 7 !");
+// Console.Clear();
+// Console.WriteLine("Введите цифру, обозначающую день недели:  ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 1 || number > 7)
+// {
+//     Console.WriteLine("Введите цифру, от 1 до 7 !");
 
 
-    while (number < 1 || number > 7)
-    {
-        int number1 = Convert.ToInt32(Console.ReadLine());
-        if (number1 > 0 && number1 < 6)
-        {
-            Console.WriteLine(" Будний день:(");
-        return;
-        }
-        if (number1 == 6 || number1 == 7)
-        {
-            Console.WriteLine(" УРА!!! ВЫХОДНОЙ:) ");
-        return;
-        }
-         if   (number < 1 || number > 7)
-         {
-             Console.WriteLine("Введите цифру, от 1 до 7 !");
-         }
-    }
-}
-    if (number > 0 && number < 6)
-    {
-        Console.WriteLine(" Будний день:(");
-    }
-    if (number == 6 || number == 7)
-    {
-        Console.WriteLine(" УРА!!! ВЫХОДНОЙ:) ");
-    }
+//     while (number < 1 || number > 7)
+//     {
+//         int number1 = Convert.ToInt32(Console.ReadLine());
+//         if (number1 > 0 && number1 < 6)
+//         {
+//             Console.WriteLine(" Будний день:(");
+//         return;
+//         }
+//         if (number1 == 6 || number1 == 7)
+//         {
+//             Console.WriteLine(" УРА!!! ВЫХОДНОЙ:) ");
+//         return;
+//         }
+//          if   (number < 1 || number > 7)
+//          {
+//              Console.WriteLine("Введите цифру, от 1 до 7 !");
+//          }
+//     }
+// }
+// if (number > 0 && number < 6)
+// {
+//     Console.WriteLine(" Будний день:(");
+// }
+// if (number == 6 || number == 7)
+// {
+//     Console.WriteLine(" УРА!!! ВЫХОДНОЙ:) ");
+// }
 
