dacf139eb7ec1f4c26e6aba20a6126d7481a1b97
Valeriy <cherkasov77@mail.ru>
2022-08-13 11:42:04 +0400

    Семинар2 задача10

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход 
 //трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-//Console.WriteLine("Введите трехзначное число:  ");
-//int number = Convert.ToInt32(Console.ReadLine());
-//if (number > 99 && number < 1000)
-//   Console.WriteLine($" {(number%1000-number%10)%number%100/10}");
-//else
-//{
-//    Console.WriteLine("Вводите трехзначное число!");
-//}
+Console.WriteLine("Введите трехзначное число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+  Console.WriteLine($" {(number-number%10)%number%100/10}");
+else
+{
+   Console.WriteLine("Вводите трехзначное число!");
+}
 
 //Задача 13:Напишите программу, которая выводит третью цифру 
 // заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите трехзначное число:  ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number > 99 && number < 1000)
-    Console.WriteLine($" {number % 10}");
-else
-{
-    Console.WriteLine("Третьего числа нет!");
-}
+// Console.WriteLine("Введите трехзначное число:  ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 99)
+//     Console.WriteLine($" {number % 10}");
+// else
+// {
+//     Console.WriteLine("Третьего числа нет!");
+// }
